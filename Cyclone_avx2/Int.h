2c0e0d9068370072449c1266300311b259dc089f
Tech priest <imorozov999@gmail.com>
2025-05-03 14:13:49 +0300

    Update Int.h

--- Cyclone_avx2/Int.h
+++ Cyclone_avx2/Int.h
@@ -1,22 +1,3 @@
-/*
- * This file is part of the BSGS distribution (https://github.com/JeanLucPons/VanitySearch).
- * Copyright (c) 2020 Jean Luc PONS.
- *
- * This program is free software: you can redistribute it and/or modify
- * it under the terms of the GNU General Public License as published by
- * the Free Software Foundation, version 3.
- *
- * This program is distributed in the hope that it will be useful, but
- * WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
- * General Public License for more details.
- *
- * You should have received a copy of the GNU General Public License
- * along with this program. If not, see <http://www.gnu.org/licenses/>.
-*/
-
-// Big integer class (Fixed size)
-
 #ifndef BIGINTH
 #define BIGINTH
 #include "Random.h"
@@ -89,7 +70,7 @@ public:
   bool IsStrictPositive();
   bool IsPositive();
   bool IsNegative();
-  bool IsEven();
+  bool IsEven() const;
   bool IsOdd();
   bool IsProbablePrime();
 
